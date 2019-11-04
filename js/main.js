28fd38d962f495a7e06a4ad1efefee590e046e82
najmaA <najmaamin10@gmai.com>
2019-11-04 09:46:02 +0300

    added hide function in p1,p2,p3

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+  $("#p1").hide();
+  $("#p2").hide();
+  $("#p3").hide();
+
   $("#image1").click(function() {
     $("#p1").show();
   });
@@ -34,5 +38,25 @@ $(document).ready(function() {
   });
   $("#p3").click(function() {
     $("#p3").hide();
+  });
+  $("#work3").hover(function() {
+    $("#text3").show();
+  });
+  $("#work3").mouseleave(function() {
+    $("#text3").hide();
+  });
+
+  $("#work2").hover(function() {
+    $("#text2").show();
+  });
+  $("#work2").mouseleave(function() {
+    $("#text2").hide();
+  });
+
+  $("#work1").hover(function() {
+    $("#text1").show();
+  });
+  $("#work1").mouseleave(function() {
+    $("#text1").hide();
   });
 });
