901be5932047c93926defd519ecfdf09e25b3c93
najmaA <najmaamin10@gmai.com>
2019-11-04 09:21:51 +0300

    js functin for product management

--- js/main.js
+++ js/main.js
@@ -27,95 +27,12 @@ $(document).ready(function() {
     $("#p3").show();
   });
   $("#image3").click(function() {
-    $("#image2").hide();
+    $("#image3").hide();
   });
-  $("#p2").click(function() {
-    $("#image2").show();
+  $("#p3").click(function() {
+    $("#image3").show();
   });
-  $("#p2").click(function() {
-    $("#p2").hide();
+  $("#p3").click(function() {
+    $("#p3").hide();
   });
-  // $("div.development").click(function() {
-  //   $(".swapin1").toggle();
-  //   $(".swapout1").toggle();
-  // });
-
-  // $("div.product").click(function() {
-  //   $(".swapin2").toggle();
-  //   $(".swapout2").toggle();
-  // });
-
-  // $("form").submit(function() {
-  //   var name = $("input#user").val();
-  //   var email = $("input#email").val();
-  //   var text = $("input#textarea").val();
-
-  //   if (
-  //     $("input#user").val() &&
-  //     $("input#email").val() &&
-  //     $("input#textarea").val !== ""
-  //   ) {
-  //     alert(name + ", we appreciate you reaching out. Thank you!");
-  //   } else {
-  //     alert("Please fill out the feedback form, Thank you.");
-  //   }
-
-  //   event.preventDefault();
-  // });
-
-  // $("#work4").hover(function() {
-  //   $("#text4").show();
-  // });
-  // $("#work4").mouseleave(function() {
-  //   $("#text4").hide();
-  // });
-
-  // $("#work3").hover(function() {
-  //   $("#text3").show();
-  // });
-  // $("#work3").mouseleave(function() {
-  //   $("#text3").hide();
-  // });
-
-  // $("#work2").hover(function() {
-  //   $("#text2").show();
-  // });
-  // $("#work2").mouseleave(function() {
-  //   $("#text2").hide();
-  // });
-
-  // $("#work1").hover(function() {
-  //   $("#text1").show();
-  // });
-  // $("#work1").mouseleave(function() {
-  //   $("#text1").hide();
-  // });
-
-  // $("#work5").hover(function() {
-  //   $("#text5").show();
-  // });
-  // $("#work5").mouseleave(function() {
-  //   $("#text5").hide();
-  // });
-
-  // $("#work6").hover(function() {
-  //   $("#text6").show();
-  // });
-  // $("#work6").mouseleave(function() {
-  //   $("#text6").hide();
-  // });
-
-  // $("#work7").hover(function() {
-  //   $("#text7").show();
-  // });
-  // $("#work7").mouseleave(function() {
-  //   $("#text7").hide();
-  // });
-
-  // $("#work8").hover(function() {
-  //   $("#text8").show();
-  // });
-  // $("#work8").mouseleave(function() {
-  //   $("#text8").hide();
-  // });
 });
